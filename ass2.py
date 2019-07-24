54f6ae014668491f56979f02675b176d4fca83cb
Titiotis1 <52955356+Titiotis1@users.noreply.github.com>
2019-07-24 07:03:46 +0100

    Add files via upload

---
+++ ass2.py
@@ -0,0 +1,7 @@
+Age = int(input("enter your age"))
+if Age < 12 :
+    print("you're  lucky to be here")
+if Age is 12 <= Age <= 20:
+        print("make good use of this opportunity ")   
+if Age is > 20:
+    print("learning is a continuous  process,  start learning? ")
